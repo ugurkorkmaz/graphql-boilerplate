3c546891d9b29f358620d5723696407067066e56
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-07-17 16:24:56 +0300

    refactor: clean up app.go code and use environment variables

--- app.go
+++ app.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"app/resolver"
@@ -11,36 +14,44 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 )
 
 var (
-	_ctx context.Context
-	_chi *chi.Mux
-	_gql *handler.Server
-)
-
-var (
-	// Version is the version of the application.
-	version string = "0.0.0"
-	// Name of the application.
-	name string = "app"
-	// Port the application will listen on.
-	port string = ":8080"
-	// SecretKey is the secret key used to sign JWT tokens.
-	key string = "secret"
-	// HeaderName is the name of the header that contains the secret key.
-	headerName string = "X-App-Key"
-	// GraphQLComplexityLimit is the maximum complexity of a GraphQL query.
-	gcl int = 35
+	_ctx          context.Context
+	_chi          *chi.Mux
+	_gql          *handler.Server
+	appVersion    = "0.0.0"
+	appName       string
+	appPort       string
+	appSecretKey  string
+	appHeaderName string
+	gqlComplexity int
 )
 
 func init() {
-	// Init global variables
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Set variable values from environment
+	appName = os.Getenv("APP_NAME")
+	appPort = os.Getenv("APP_PORT")
+	appSecretKey = os.Getenv("APP_SECRET_KEY")
+	appHeaderName = os.Getenv("APP_SECRET_HEADER_NAME")
+
+	// Convert GQL_COMPLEXITY_LIMIT to integer
+	gqlComplexity, err = strconv.Atoi(os.Getenv("GQL_COMPLEXITY_LIMIT"))
+	if err != nil {
+		log.Fatal("Invalid GQL_COMPLEXITY_LIMIT value")
+	}
+
 	_ctx = context.Background()
-	// Init chi router
 	_chi = chi.NewRouter()
 }
 
@@ -50,9 +61,7 @@ func main() {
 
 	// Add routes and configure the GraphQL server
 	_gql = handler.NewDefaultServer(resolver.NewSchema())
-	// Add POST transport
 	_gql.AddTransport(transport.POST{})
-	// Add websocket transport
 	_gql.AddTransport(transport.Websocket{
 		KeepAlivePingInterval: 5 * time.Second,
 		Upgrader: websocket.Upgrader{
@@ -63,38 +72,31 @@ func main() {
 			},
 		},
 	})
-	// Add OPTIONS transport
 	_gql.AddTransport(transport.Options{})
-	// Add GET transport
 	_gql.AddTransport(transport.GET{})
-	// Add POST transport
 	_gql.AddTransport(transport.POST{})
-	// Add multipart form transport
 	_gql.AddTransport(transport.MultipartForm{
 		MaxUploadSize: 10 * 1024 * 1024,
 		MaxMemory:     32 << 20,
 	})
-	// Complexity limit
-	_gql.Use(extension.FixedComplexityLimit(gcl))
+	_gql.Use(extension.FixedComplexityLimit(gqlComplexity))
 
-	// Add routes Secured
+	// Add secured routes
 	_chi.Group(func(r chi.Router) {
 		_chi.Handle("/graphql", _gql)
 		_chi.Handle("/websocket", _gql)
-		_chi.Handle("/", playground.Handler(name, "/graphql"))
+		_chi.Handle("/", playground.Handler(appName, "/graphql"))
 	}).Use(Secret)
 
-	http.ListenAndServe(port, _chi)
+	// Start the HTTP server
+	http.ListenAndServe(appPort, _chi)
 }
 
 // GlobalContext creates a new context for each HTTP request and adds the request data to the context.
 // This middleware can be used to easily access the request data.
 func GlobalContext(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create a new context and add the request data to it using the custom struct type as key.
-		//lint:ignore SA1029 We need to use the deprecated function here.
 		_ctx = context.WithValue(r.Context(), "request", r)
-		// Call the http.Handler with the new context.
 		next.ServeHTTP(w, r.WithContext(_ctx))
 	})
 }
@@ -102,10 +104,10 @@ func GlobalContext(next http.Handler) http.Handler {
 // Secret is a middleware that checks the secret key in the header.
 func Secret(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		secretKey := r.Header.Get(headerName)
-		if secretKey != key {
+		secretKey := r.Header.Get(appHeaderName)
+		if secretKey != appSecretKey {
 			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte(`{"status":false,"message":"Unauthorized"}`))
+			w.Write([]byte(`{"status": false, "message": "Unauthorized"}`))
 			return
 		}
 		next.ServeHTTP(w, r)
